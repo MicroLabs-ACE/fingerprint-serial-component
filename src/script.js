6df5ca07bd4444d3862c825ae3ee298cd5a2e0f6
Victor Momodu <victoramomodu@gmail.com>
2024-06-14 10:01:27 +0100

    feat: Test serial ports in Python

---
+++ src/script.js
@@ -0,0 +1,4 @@
+navigator.serial.getPorts().then((ports) => {
+  // Initialize the list of available ports with `ports` on page load.
+});
+
--- src/script.ts
+++
@@ -1,6 +0,0 @@
-const serialStatus = document.createElement("p");
-
-if ("serial" in navigator) serialStatus.textContent = "Supports serial.";
-else serialStatus.textContent = "Does not support serial.";
-
-document.body.appendChild(serialStatus);
