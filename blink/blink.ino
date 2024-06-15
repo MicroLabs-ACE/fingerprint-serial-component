67c726ceca3951ac2678253f810bfb762d70876c
Victor Momodu <victoramomodu@gmail.com>
2024-06-15 09:48:48 +0100

    feat: Add writing and reading functionality

--- blink/blink.ino
+++ blink/blink.ino
@@ -2,10 +2,29 @@ const int bufferSize = 4;
 byte buffer[bufferSize];
 int index = 0;
 int wait = 1000;
+byte output[] = {
+  0x00,
+  0x01,
+  0x02,
+  0x03,
+  0x04,
+  0x05,
+  0x06,
+  0x07,
+  0x08,
+  0x09,
+  0x0A,
+  0x0B,
+  0x0C,
+  0x0D,
+  0x0E,
+  0x0F,
+};
+int outputIndex = 0;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -29,4 +48,6 @@ void processData(byte* data, int length) {
   String result = "";
   for (int i = 0; i < length; i++) result += (char)data[i];
   wait = result.toInt() * 1000;
+  Serial.println(output[outputIndex]);
+  outputIndex++;
 }
--- src/script.js
+++ src/script.js
@@ -1,28 +1,37 @@
 let port;
 let writer;
 let reader;
+let inputData;
+let outputData;
 
 document.getElementById("connect").addEventListener("click", async () => {
   try {
     port = await navigator.serial.requestPort();
     await port.open({ baudRate: 9600 });
-
     writer = port.writable.getWriter();
     reader = port.readable.getReader();
-
     document.getElementById("status").textContent = "Connected to serial port!";
-    readFromSerial();
   } catch (error) {
     document.getElementById("status").textContent = `Error: ${error}`;
   }
 });
 
 document.getElementById("sendData").addEventListener("click", async () => {
-  const inputData = document.getElementById("inputData").value;
-  const data = new TextEncoder().encode(inputData + "\n");
-  await writer.write(data);
-  document.getElementById("status").textContent = `Sent: ${inputData}`;
+  try {
+    inputData = [0xef, 10];
+    await writeToSerial(inputData);
+    readFromSerial();
+    console.log(outputData);
+    document.getElementById("status").textContent = `Sent: ${inputData}`;
+  } catch (error) {
+    document.getElementById("status").textContent = `Error: ${error}`;
+  }
 });
+
+async function writeToSerial(inputData) {
+  const data = new Uint8Array(inputData);
+  await writer.write(data);
+}
 
 async function readFromSerial() {
   while (true) {
@@ -31,8 +40,6 @@ async function readFromSerial() {
       reader.releaseLock();
       break;
     }
-
-    const receivedText = new TextDecoder().decode(value);
-    document.getElementById("status").textContent = `Received: ${receivedText}`;
+    outputData = value;
   }
 }
