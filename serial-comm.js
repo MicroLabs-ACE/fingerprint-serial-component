b5b2ee7a72b9bf376f7ef7c6ac5ae766789bc096
Victor Momodu <victoramomodu@gmail.com>
2024-06-23 18:14:12 +0100

    fix: Correct checksum implementation to compute 2 bytes

--- serial-comm.js
+++ serial-comm.js
@@ -1,5 +1,4 @@
 const os = require("os");
-const { Buffer } = require("node:buffer");
 
 const { autoDetect } = require("@serialport/bindings-cpp");
 const { SerialPort } = require("serialport");
@@ -13,11 +12,34 @@ const { Commands } = require("./commands");
 //   });
 // });
 
-const serialport = new SerialPort({ path: "/dev/ttyUSB0", baudRate: 57600 });
-const writeResult = serialport.write(
-  Buffer.from(Commands.GET_RANDOM_CODE),
-  "hex"
+const serialport = new SerialPort(
+  { path: "/dev/ttyUSB0", baudRate: 57600 },
+  (err) => {
+    if (err) return console.error("Error opening port:", err.message);
+    console.log(`Port opened.`);
+    console.log(`Write: ${writeToSerial(Commands.GET_RANDOM_CODE)}`);
+    console.log(`Read: ${readFromSerial()}`);
+    serialport.close();
+  }
 );
-serialport.drain();
 
-console.log(writeResult);
+function computeChecksum(byteArray) {
+  let sum = 0;
+  for (let i = 0; i < byteArray.length; i++) sum += byteArray[i];
+  return [0x00, sum % 63];
+}
+
+function writeToSerial(command) {
+  const checksum = computeChecksum(command);
+  const commandAndChecksum = [...command, ...checksum];
+  console.log(commandAndChecksum.length);
+  const inputData = new Uint8Array(commandAndChecksum);
+  const result = serialport.write(inputData);
+
+  return result;
+}
+
+function readFromSerial() {
+  const outputData = serialport.read(12);
+  return outputData;
+}
